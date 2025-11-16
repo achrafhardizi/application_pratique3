d45135618129c3d0d6a8f1164430206f59420f60
achrafhardizi <achrafhardizi1002@gmail.com>
2025-11-16 18:07:03 +0100

    init : config svc

---
+++ config-svc/src/main/java/com/achraf/configsvc/ConfigSvcApplication.java
@@ -0,0 +1,13 @@
+package com.achraf.configsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSvcApplication.class, args);
+    }
+
+}
---
+++ config-svc/src/test/java/com/achraf/configsvc/ConfigSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achraf.configsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
