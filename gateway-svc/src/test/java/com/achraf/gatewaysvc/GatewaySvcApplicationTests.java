75ac66c100518047992e047c9ea045d323e45da7
achrafhardizi <achrafhardizi1002@gmail.com>
2025-11-16 18:02:29 +0100

    init : gateway svc

---
+++ gateway-svc/src/test/java/com/achraf/gatewaysvc/GatewaySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achraf.gatewaysvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
