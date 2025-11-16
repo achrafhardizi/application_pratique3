d2029b3800ed809350cdf6d0a9e17d9a81c2ae0f
achrafhardizi <achrafhardizi1002@gmail.com>
2025-11-16 18:25:33 +0100

    init : customer svc

--- config-svc/src/main/java/com/achraf/configsvc/ConfigSvcApplication.java
+++ config-svc/src/main/java/com/achraf/configsvc/ConfigSvcApplication.java
@@ -2,8 +2,10 @@ package com.achraf.configsvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigSvcApplication {
 
     public static void main(String[] args) {
